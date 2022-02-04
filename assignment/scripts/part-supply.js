a222930ec42ac74b10ee39682c0c6383db06b0c3
Michael Chuinard <mpchuinard@finlandia.edu>
2022-02-03 18:39:39 -0600

    Completed question 6 with a for loop and conditional statement

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -9,7 +9,7 @@ console.log(partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
-let supplyChanges = ['3', '5', '-6', '0', '7', '11'];
+let supplyChanges = [3, 5, -6, 0, 7, 11];
 console.log(supplyChanges);
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
@@ -21,13 +21,22 @@ let removedItem = supplyChanges.pop();
 console.log(removedItem);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-supplyChanges.push('25');
+supplyChanges.push(25);
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
+for (let x=0; x<supplyChanges.length; x++) {
+  if (supplyChanges[x] > 0) {
+    console.log(`Added ${supplyChanges[x]} parts.`);
+  } else if (supplyChanges[x] === 0) {
+    console.log('No Change.');
+  } else {
+    console.log(`Removed ${supplyChanges[x]} parts.`);
+  }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
@@ -37,6 +46,7 @@ console.log('7. Showing supplyChanges with "for of" loop');
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+
 
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
