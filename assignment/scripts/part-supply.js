32beed44c0fe53b018fed75e90ef95099e003a64
Michael Chuinard <mpchuinard@finlandia.edu>
2022-02-03 22:33:07 -0600

    Completed stretch goal question 8 by using a for loop to determine the sum of the supplyChanges array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -54,6 +54,11 @@ for (x of supplyChanges) {
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let sum = 0;
+for (let i=0; i<supplyChanges.length; i++) {
+    sum+=supplyChanges[i];
+}
+console.log(sum);
 
 
 // 9. We have a large stash of parts in our warehouse that we
